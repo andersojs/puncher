8f36d3e9c12e83ea626d3bcec11711069472995f
Jonathan Anderson <andersoj@andersoj.org>
2025-11-25 00:03:01 -0500

    More import and dynlib handling

--- src/puncher/__init__.py
+++ src/puncher/__init__.py
@@ -1,9 +1,13 @@
+import sys 
+
 try:
     import cairosvg
 except OSError as e:
-    print(f"An OS error occurred during import: {e}, please make sure CairoSVG is installed and on the dynamic library path.")
+    print(f"An OS error occurred during import: {e}, please make sure CairoSVG is installed and on the dynamic library path.", file=sys.stderr)
+    print(f"On Mac with cairosvg installed using Homebrew, I need:\nxport DYLD_LIBRARY_PATH=/opt/homebrew/lib", file=sys.stderr)
     # You can add specific handling logic here,
     # such as providing a fallback or logging the error.
+    sys.exit(-1)
 except ImportError as e:
     print(f"An import error occurred: {e}")
     # Handle cases where the module simply isn't found
